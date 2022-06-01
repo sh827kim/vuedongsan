68250488f9fca965011a49676fe71114b34f5889
seonghwa.kim <sh827kim@gmail.com>
2022-06-02 08:59:28 +0900

    vuedongsan first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <div class="black-bg" v-if="isModalOpen==true">
+      <div class="white-bg">
+        <h4>Details</h4>
+        <p>Detail contents</p>
+        <button @click="isModalOpen=false">close</button>
+      </div>
+    </div>
+    <div class="menu">
+      <a v-for="(menu, idx) in menus" :key="idx">{{menu}}</a>
+    </div>
+    <div v-for="product in products" :key="product">
+      <img :src="product.image" class="room-img"/>
+      <h4 @click="isModalOpen=true">{{product.title}}</h4>
+      <p>{{product.price}}원</p>
+      <button @click="addCount(product)">허위 매물 신고</button> <span>신고수 : {{product.count}}</span>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import products from './assets/post.js'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menus: [`Home`, `Products`, `About`],
+      products : products,
+      isModalOpen: false,
+    }
+  },
+  methods : {
+    addCount(product) {
+      product.count+=1;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +50,44 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin : 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 </style>
